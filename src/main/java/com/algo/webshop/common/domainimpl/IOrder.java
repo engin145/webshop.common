aa1f6852ea76d188b320d2a5d327ddd8d85c33e7
KIKEP1990 <kickker90@gmail.com>
2013-10-28 18:42:59 +0200

    fix orders.jsp

--- src/main/java/com/algo/webshop/common/domainimpl/IOrder.java
+++ src/main/java/com/algo/webshop/common/domainimpl/IOrder.java
@@ -1,13 +1,12 @@
 package com.algo.webshop.common.domainimpl;
 
-import java.util.Calendar;
 import java.util.List;
 import java.util.Set;
 
 import com.algo.webshop.common.domain.Order;
 
 public interface IOrder {
-	public Order getOrder(int id);
+	public Order getOrderById(int id);
 
 	public Order getOrder(String number);
 
@@ -15,16 +14,12 @@ public interface IOrder {
 
 	public List<Order> getOrders(int confirmStatus, int canselStatus);
 
-	public Set<Order> getOrders(Calendar dateFrom, Calendar dateTo,
-			int status_pay, int status_release, int status_cansel,
-			int status_confirm);
-
 	public void addOrder(Order order);
 
 	public void updateOrder(Order order);
 
 	public String getLastOrderNumber();
-	
+
 	public int getOrderIdByNumber(String number);
 
 }
